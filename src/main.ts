825e7c228d994940875a1894f3bd561a6c6952d8
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-04-25 21:19:39 +0900

    feat: Swagger 패키지 설치 및 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정 추가
+  const config = new DocumentBuilder()
+    .setTitle('[프로젝트 이름] API')
+    .setDescription('[프로젝트 이름] API 문서')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT 입력창 추가
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // http://localhost:3000/api-docs
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
